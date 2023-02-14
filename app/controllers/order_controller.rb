68adf494b6ba3de28e40898251ddb5c08b416c8e
Dustin Milam <darklordsushi@yahoo.com>
2023-02-13 22:02:32 -0500

    undid testing code

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -10,9 +10,9 @@ class OrderController < ApplicationController
     if verified
 
       for line_item in params["line_items"]
-        product = ShopifyAPI::Product.find id: 8129157857554 # <==== test product
+        # product = ShopifyAPI::Product.find id: 8129157857554 # <==== test product
         # get the Shopify Product purchased in this line item
-        # product = ShopifyAPI::Product.find id: line_item["product_id"]  # qw12 TESTING!!
+        product = ShopifyAPI::Product.find id: line_item["product_id"]  # qw12 TESTING!!
 
         # identify the tube and carton variant 
         carton_variant = product.variants.select{|v| v.option1&.downcase == "carton" or v.option2&.downcase == "carton" or v.option3&.downcase == "carton"} .first
@@ -24,7 +24,7 @@ class OrderController < ApplicationController
         tubes_per_carton = tubes_per_carton_metafield.value if tubes_per_carton_metafield
 
         puts Colorize.cyan("tubes_per_carton: #{tubes_per_carton}, current/carton id: #{line_item["variant_id"]}/#{carton_variant}")
-        if tubes_per_carton or line_item["variant_id"] == carton_variant  # qw12 TESTING!! OR/AND
+        if tubes_per_carton and line_item["variant_id"] == carton_variant  # qw12 TESTING!! OR/AND
           order = Order.find_by_order_id params["order"]["id"]
           unless order
             # get InventoryLevel object for the tube variant
@@ -64,9 +64,9 @@ class OrderController < ApplicationController
     if verified
       for refund_item in params["refund_line_items"]
         line_item = refund_item["line_item"]
-        product = ShopifyAPI::Product.find id: 8129157857554 # <==== test product
+        # product = ShopifyAPI::Product.find id: 8129157857554 # <==== test product
         # get the Shopify Product purchased in this line item
-        # product = ShopifyAPI::Product.find id: line_item["product_id"] # qw12 TESTING!!
+        product = ShopifyAPI::Product.find id: line_item["product_id"] # qw12 TESTING!!
 
         # identify the tube and carton variant 
         carton_variant = product.variants.select{|v| v.option1&.downcase == "carton" or v.option2&.downcase == "carton" or v.option3&.downcase == "carton"} .first
@@ -78,7 +78,7 @@ class OrderController < ApplicationController
         tubes_per_carton = tubes_per_carton_metafield.value if tubes_per_carton_metafield
 
         puts Colorize.cyan("tubes_per_carton: #{tubes_per_carton}, current/carton id: #{line_item["variant_id"]}/#{carton_variant}")
-        if tubes_per_carton or line_item["variant_id"] == carton_variant or refund_item["restock_type"] != "no_restock" # qw12 TESTING!! OR/AND
+        if tubes_per_carton and line_item["variant_id"] == carton_variant and refund_item["restock_type"] != "no_restock" # qw12 TESTING!! OR/AND
           order = Order.find_by_order_id "re_#{params["order"]["id"]}"
           unless order
             # get InventoryLevel object for the tube variant
